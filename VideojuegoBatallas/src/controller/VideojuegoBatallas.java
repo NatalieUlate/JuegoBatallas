fcc01ba42b7a2c1765d99ea050668f6f80e14bb1
NataieUlate <ulaterojasnatalie@gmail.com>
2025-08-07 20:39:38 -0600

    Instancia de la clase MotorJuego dentro del método main

--- VideojuegoBatallas/src/controller/VideojuegoBatallas.java
+++ VideojuegoBatallas/src/controller/VideojuegoBatallas.java
@@ -14,7 +14,8 @@ public class VideojuegoBatallas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MotorJuego juego = new MotorJuego();
+        juego.iniciar();
     }
     
 }
